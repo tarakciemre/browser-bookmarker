4bcc4caa64d0cc6b40488f0b6da75d21f63eb41e
Yarkın Sakıncı <yarkinnsakinci@gmail.com>
2023-12-07 21:01:57 +0300

    add new and remove tab functions

--- src/tabactions.ts
+++ src/tabactions.ts
@@ -1,37 +1,37 @@
 class TabManager {
-    private tabsContainer: HTMLElement;
-    private addTabButton: HTMLElement;
-  
-    constructor() {
-      this.tabsContainer = document.querySelector('.tab-bar')!;
-      this.addTabButton = document.querySelector('.add-tab-button')!; 
-      this.addTabButton.addEventListener('click', () => this.addNewTab());
-    }
+  private tabsContainer: HTMLElement;
+  private addTabButton: HTMLElement;
+  private tabCloseButton: HTMLElement;
 
+  constructor() {
+    this.tabsContainer = document.querySelector('.tab-bar')!;
+    this.addTabButton = document.querySelector('.add-tab-button')!; 
+    this.addTabButton.addEventListener('click', () => this.addNewTab());
+    this.tabsContainer.querySelectorAll('.tab button').forEach((closeButton) => {
+      closeButton.addEventListener('click', () => this.closeTab(closeButton.parentElement!));
+    });
+  }  
+  private addNewTab() {
+    const tab = document.createElement('div');
+    tab.className = 'tab';
 
-  
-    private addNewTab() {
-      const tab = document.createElement('div');
-      tab.className = 'tab';
-  
-      const title = document.createElement('h4');
-      title.textContent = 'Title';
-  
-      const closeButton = document.createElement('button');
-      closeButton.textContent = 'x';
-      closeButton.addEventListener('click', () => this.closeTab(tab));
-      tab.appendChild(title);
-      tab.appendChild(closeButton);
-      this.tabsContainer.insertBefore(tab, this.addTabButton);
-    }
-  
-    private closeTab(tab: HTMLElement) {
-      this.tabsContainer.removeChild(tab);
-    }
+    const title = document.createElement('h4');
+    title.textContent = 'Title';
+
+    const closeButton = document.createElement('button');
+    closeButton.textContent = 'x';
+    closeButton.addEventListener('click', () => this.closeTab(tab));
+    tab.appendChild(title);
+    tab.appendChild(closeButton);
+    this.tabsContainer.insertBefore(tab, this.addTabButton);
   }
-  
 
-  document.addEventListener('DOMContentLoaded', () => {
-    const tabManager = new TabManager();
-  });
-  
+  private closeTab(tab: HTMLElement) {
+    this.tabsContainer.removeChild(tab);
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const tabManager = new TabManager();
+});
